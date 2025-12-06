af01f22fd7914dbabe50075bf79b6300bf297b33
= <devwithmpho@gmail.com>
2025-12-06 14:48:35 +0200

    setup project

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from 'next';
+import { Montserrat } from 'next/font/google';
+import './globals.css';
+
+const montserratFont = Montserrat({
+	subsets: ['latin'],
+	weight: '400',
+	variable: '--font-montserrat',
+});
+
+export const metadata: Metadata = {
+	title: 'AutoWorx',
+	description:
+		'Reliable, affordable mechanical service designed to keep your vehicle running at its best. From routine maintenance to full repairs, we get you back on the road with confidence.',
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang='en'>
+			<body className={`${montserratFont.className} antialiased`}>
+				{children}
+			</body>
+		</html>
+	);
+}
